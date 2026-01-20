f7a2ad566a8f503b9cdc1608a9d99bd637686491
CorcundaPT <79412616+CorcundaPT@users.noreply.github.com>
2026-01-20 12:47:04 +0000

    update files
    - add new features
    - add new spells
    - add new items

--- data/features.js
+++ data/features.js
@@ -14,6 +14,11 @@ const feature = {
 		"type":"feat",
 		"effect":["You gain the following benefits.","<b class='featureInfoBold'>Initiative Proficiency:</b> When you roll Initiative, you can add your Proficiency Bonus to the roll.","<b class='featureInfoBold'>Initiative Swap:</b> Immediately after you roll Initiative, you can swap your Initiative with the Initiative of one willing ally in the same combat. You can't make this swap if you or the ally has the Incapacitated condition."]
 	},
+	"channelDivinity": {
+		"name":"Channel Divinity",
+		"type":"class feature",
+		"effect":["You can channel divine energy directly from the Outer Planes to fuel magical effects. You start with two such effects: Divine Spark and Turn Undead, each of which is described below.","You can use this class's Channel Divinity twice. You regain one of its expended uses when you finish a Short Rest, and you regain all expended uses when you finish a Long Rest. You gain additional uses when you reach certain Cleric levels, as shown in the Channel Divinity column of the Cleric Features table.","If a Channel Divinity effect requires a saving throw, the DC equals the spell save DC from this class's Spellcasting feature.","<b class='featureInfoBold'>Divine Spark</b> As a Magic action, you point your Holy Symbol at another creature you can see within 30 feet of yourself and focus divine energy at it. Roll 1d8 and add your Wisdom modifier. You either restore Hit Points to the creature equal to that total or force the creature to make a Constitution saving throw. On a failed save, the creature takes Necrotic or Radiant damage (your choice) equal to that total. On a successful save, the creature takes half as much damage (round down).","You roll an additional d8 when you reach Cleric levels 7 (2d8), 13 (3d8), and 18 (4d8).","<b class='featureInfoBold'>Turn Undead</b> As a Magic action, you present your Holy Symbol and censure Undead creatures. Each Undead of your choice within 30 feet of you must make a Wisdom saving throw. If the creature fails its save, it has the Frightened and Incapacitated conditions for 1 minute. For that duration, it tries to move as far from you as it can on its turns. This effect ends early on the creature if it takes any damage, if you have the Incapacitated condition, or if you die."]
+	},
 	"cunningAction": {
 		"name":"Cunning Action",
 		"type":"class feature",
@@ -28,6 +33,11 @@ const feature = {
 		"name":"Darkvision",
 		"type":"specie feature",
 		"effect":["You have Darkvision with a range of 120ft."]
+	},
+	"divineOrder": {
+		"name":"Divine Order",
+		"type":"class feature",
+		"effect":["You have dedicated yourself to one of the following sacred roles of your choice.","<b class='featureInfoBold'>Protector</b> Trained for battle, you gain proficiency with Martial weapons and training with Heavy armor.","<b class='featureInfoBold'>Thaumaturge</b> You know one extra cantrip from the Cleric spell list. In addition, your mystical connection to the divine gives you a bonus to your Intelligence (Arcana or Religion) checks. The bonus equals your Wisdom modifier (minimum of +1)."]
 	},
 	"divineSpark": {
 		"name":"Channel Divinity: Divine Spark",
@@ -80,6 +90,11 @@ const feature = {
 		"name":"Sneak Attack",
 		"type":"class feature",
 		"effect":["Once per turn, you can deal an extra 1d6 damage to one creature you hit with an attack roll if you have Advantage on the roll and the attack uses a Finesse or a Ranged weapon. The extra damage's type is the same as the weapon's type.","You don't need Advantage on the attack roll if at least one of your allies is within 5ft of the target, the ally doesn't have the Incapacitated condition, and you don't have Disadvantage on the attack roll.","The extra damage increases as you gain Rogue levels, as shown in the Sneak Attack column of the Rogue Features table. (one extra die every two levels)"]
+	},
+	"spellcastingCleric": {
+		"name":"Spellcasting",
+		"type":"class feature",
+		"effect":["You have learned to cast spells through prayer and meditation.","<b class='featureInfoBold'>Cantrips</b> You know three cantrips of your choice from the Cleric spell list.","Whenever you gain a Cleric level, you can replace one of your cantrips with another cantrip of your choice from the Cleric spell list.","When you reach Cleric levels 4 and 10, you learn another cantrip of your choice from the Cleric spell list, as shown in the Cantrips column of the Cleric Features table.","<b class='featureInfoBold'>Spell Slots</b> The Cleric Features table shows how many spell slots you have to cast your spells. You regain all expended slots when you finish a Long Rest.","<b class='featureInfoBold'>Prepared Spells</b> You prepare the list of spells that are available for you to cast with this feature. To start, choose four level 1 spells from the Cleric spell list.","The number of spells on your list increases as you gain Cleric levels, as shown in the Prepared Spells column of the Cleric Features table.","If another Cleric feature gives you spells that you always have prepared, those spells don't count against the number of spells you can prepare with this feature, but those spells otherwise count as Cleric spells for you.","<b class='featureInfoBold'>Changing Your Prepared Spells</b> Whenever you finish a Long Rest, you can change your list of prepared spells, replacing any of the spells there with other Cleric spells for which you have spell slots.","<b class='featureInfoBold'>Spellcasting Ability</b> Wisdom is your spellcasting ability for your Cleric spells.","<b class='featureInfoBold'>Spellcasting Focus</b> You can use a Holy Symbol as a Spellcasting Focus for your Cleric spells."]
 	},
 	"thievesCant": {
 		"name":"Thieves' Cant",
--- data/spells.js
+++ data/spells.js
@@ -9,6 +9,16 @@ const spell = {
 		"effect":["A creature you touch regains a number of Hit Points equal to 2d8 plus your spellcasting ability modifier."],
 		"upgrade":"The healing increases by 2d8 for each spell slot level above 1."
 	},
+	"dancingLights": {
+		"name":"Dancing Lights",
+		"type":"Illusion Cantrip",
+		"castingTime":"Action",
+		"range":"120ft",
+		"components":"V,S,M (a bit of phosphorus)",
+		"duration":"Concentration, up to 1 minute",
+		"effect":["You create up to four torch-size lights within range, making them appear as torches, lanterns, or glowing orbs that hover for the duration. Alternatively, you combine the four lights into one glowing Medium form that is vaguely humanlike. Whichever form you choose, each light sheds Dim Light in a 10-foot radius.","As a Bonus Action, you can move the lights up to 60 feet to a space within range. A light must be within 20 feet of another light created by this spell, and a light vanishes if it exceeds the spell's range."],
+		"upgrade":""
+	},
 	"fireBolt": {
 		"name":"Fire Bolt",
 		"type":"Evocation Cantrip",
