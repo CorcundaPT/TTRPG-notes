4a8e0c0b27b0f76919604a4acd73aae18462eade
CorcundaPT <79412616+CorcundaPT@users.noreply.github.com>
2026-02-17 18:17:09 +0000

    update files
    Add ana character
    add sage background

--- characters/Ana-NoName.js
+++ characters/Ana-NoName.js
@@ -1,67 +1,67 @@
 const character = {
 	"name": "NoName",
-	"specie": "Aasimar",
+	"specie": "Wood Elf",
 	"size":"Medium",
-	"background": "Noble",
+	"background": "Sage",
 	"class": [
-		{"name":"Fighter", "level":3, "subclass":"none"}
+		{"name":"Ranger", "level":3, "subclass":"none"}
 	],
-	"speed":"30ft",
+	"speed":"35ft",
 	"iniciativeBonus":0,
 	"armorClass":{
-		"armor":13,
+		"armor":12,
 		"shield":0,
-		"type":"medium",
+		"type":"light",
 		"bonus":0
 	},
 	"hitPoints":36,
 	"ability": {
-		"strength": 16,
-		"dexterity": 14,
-		"constitution": 13,
-		"intelligence": 10,
-		"wisdom": 12,
-		"charisma": 10
+		"strength": 10,
+		"dexterity": 15,
+		"constitution": 14,
+		"intelligence": 14,
+		"wisdom": 14,
+		"charisma": 8
 	},
 	"spellcasting":"none",
 	"save": {
 		"strength":		{"proficiency": 1, "bonus": 0},
-		"dexterity":	{"proficiency": 0, "bonus": 0},
-		"constitution":	{"proficiency": 1, "bonus": 0},
+		"dexterity":	{"proficiency": 1, "bonus": 0},
+		"constitution":	{"proficiency": 0, "bonus": 0},
 		"intelligence":	{"proficiency": 0, "bonus": 0},
 		"wisdom":		{"proficiency": 0, "bonus": 0},
 		"charisma":		{"proficiency": 0, "bonus": 0}
 	},
 	"skill": {
-		"athletics":		{"proficiency": 1, "bonus": 0},
+		"athletics":		{"proficiency": 0, "bonus": 0},
 		
-		"acrobatics":		{"proficiency": 1, "bonus": 0},
+		"acrobatics":		{"proficiency": 0, "bonus": 0},
 		"sleightOfHand":	{"proficiency": 0, "bonus": 0},
 		"stealth":			{"proficiency": 1, "bonus": 0},
 		
-		"arcana":			{"proficiency": 0, "bonus": 0},
+		"arcana":			{"proficiency": 1, "bonus": 0},
 		"history":			{"proficiency": 1, "bonus": 0},
 		"investigation":	{"proficiency": 0, "bonus": 0},
-		"nature":			{"proficiency": 0, "bonus": 0},
+		"nature":			{"proficiency": 1, "bonus": 0},
 		"religion":			{"proficiency": 0, "bonus": 0},
 		
-		"animalHandling":	{"proficiency": 0, "bonus": 0},
-		"insight":			{"proficiency": 1, "bonus": 0},
+		"animalHandling":	{"proficiency": 1, "bonus": 0},
+		"insight":			{"proficiency": 0, "bonus": 0},
 		"medicine":			{"proficiency": 0, "bonus": 0},
 		"perception":		{"proficiency": 1, "bonus": 0},
-		"survival":			{"proficiency": 0, "bonus": 0},
+		"survival":			{"proficiency": 1, "bonus": 0},
 		
 		"deception":		{"proficiency": 0, "bonus": 0},
 		"intimidation":		{"proficiency": 0, "bonus": 0},
 		"performance":		{"proficiency": 0, "bonus": 0},
-		"persuasion":		{"proficiency": 1, "bonus": 0}
+		"persuasion":		{"proficiency": 0, "bonus": 0}
 	},
 	"proficiency": {
 		"weapons":"Simple Weapons, Martial Weapons",
-		"armors":"Light Armor; Medium Armor; Heavy Armor; Shield",
-		"tools":"one Gaming Set",
-		"senses":"Darkvision 60ft",
-		"languages":"Common; Celestial"
+		"armors":"Light Armor; Medium Armor; Shield",
+		"tools":"Calligrapher's Supplies",
+		"senses":"Darkvison 60ft",
+		"languages":"Common; Elvish"
 	},
 	"coin": {
 		"copper":0,
